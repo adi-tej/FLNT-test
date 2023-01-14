2da6e5ae2695db374a780c26d2d2e970907d333a
ADI TEJA DASARI <gday@aditeja.com>
2023-01-14 12:30:30 +1100

    adding vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue/dist/vue.esm-bundler';
-import router from "./router";
+import { createApp } from 'vue/dist/vue.esm-bundler'
+import router from "./router"
+import vuetify from "@/plugins/vuetify"
+import "vuetify/dist/vuetify.min.css"
 
-const app = createApp({})
+const app = createApp({
+    vuetify
+})
 app.use(router)
 app.mount('#app')

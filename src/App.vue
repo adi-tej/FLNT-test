2184b09cb1308fe99695faa9d4ca3089747dda2e
ADI TEJA DASARI <gday@aditeja.com>
2023-01-16 18:05:49 +1100

    final

--- src/App.vue
+++ src/App.vue
@@ -1,149 +1,58 @@
 <template>
   <v-app>
-    <v-main>
-      <v-form v-model="valid" ref="form" @submit.prevent="add">
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-text-field
-                      v-model="name"
-                      :rules="nameRules "
-                      :counter="10"
-                      label="Item Name"
-                      required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field
-                      v-model="quantity"
-                      :rules="quantityRules"
-                      :counter="10"
-                      label="Quantity"
-                      required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-btn
-                type="submit"
-                color="success"
-                class="mr-4"
-                size="x-large"
-                :disabled="!valid"
-              >
-                {{submitButton}}
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-      <h2 class="text-center">Groceries</h2>
-      <v-divider></v-divider>
-        <v-table fixed-header density="compact">
-          <thead>
-            <tr>
-              <th class="text-center">
-                Item Name
-              </th>
-              <th class="text-center">
-                Quantity
-              </th>
-              <th class="text-center">
-                Click to remove
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-          <tr
-            v-for="(item,index) in groceries"
-            :key="index"
-            :item="item"
-          >
-            <td class="text-center">
-              {{ item.name }}
-            </td>
-            <td class="text-center">
-              {{ item.quantity }}
-            </td>
-            <td class="text-center">
+      <div class="h-100">
+        <v-row align="start h-100" no-gutters >
+          <v-col class="form-container h-100 v-col-sm">
+            <v-img class="logo" src="./assets/logo.png" height="150px"/>
+            <ItemForm submit-button="Add" />
+          </v-col>
+          <v-col class="table-container h-100 v-col-sm">
+            <v-divider></v-divider>
+            <GroceriesTable ref="table"/>
+          </v-col>
 
-              <v-btn
-                class="ma-2"
-                variant="text"
-                icon="mdi-pencil"
-                color="blue"
-                @click="edit"
-              ></v-btn>
-              <v-btn
-                class="ma-2"
-                variant="text"
-                icon="mdi-close"
-                color="error"
-                @click="remove(index)"
-              ></v-btn>
-            </td>
-          </tr>
-          </tbody>
-        </v-table>
-    </v-main>
+        </v-row>
+      </div>
+
+
+    <v-footer class="pa-0 ma-0">
+      <div class="bg-black text-center w-100">
+        {{ new Date().getFullYear() }} — <strong>Adi Teja Dasari</strong>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import ItemForm from "@/components/ItemForm";
+import GroceriesTable from "@/components/GroceriesTable";
 export default {
   name: 'App',
+  components: {GroceriesTable, ItemForm},
   data () {
     return {
       submitButton: 'Add',
       editForm: false,
-      groceries: [{
-        name: 'apple',
-        quantity: 5
-      },{
-        name: 'ball',
-        quantity: 4
-      }],
-      valid: false,
-      name: '',
-      nameRules: [
-        v => !!v || 'Item name is required',
-        v => this.editForm || (!this.groceries.find(o => o.name === v) || 'Item already exists')
-      ],
-      quantity: '',
-      quantityRules: [
-        v => !!v || 'Quantity is required',
-        v => /^-?\d+$/.test(v) || 'Quantity should be integer',
-      ],
+      editIndex: 0,
+      groceries: [],
     }
   },
-  methods: {
-    add (e) {
-      if(this.editForm){
-
-      }else{
-        this.groceries.push({
-          name: this.name,
-          quantity: this.quantity
-        })
-      }
-      this.$refs.form.reset();
-    },
-    remove (index) {
-      console.log(index)
-      this.groceries.splice(index,1)
-    },
-    edit (index) {
-      this.submitButton = 'Update'
-      this.editForm = true
-      const item = this.groceries.at(index)
-      this.name = item.name
-      this.quantity = item.quantity
-      console.log('editing')
-    }
-  },
-  watch: {
-    dialog() {
-      this.$refs.form.reset()
-    }
+  mounted() {
+    localStorage.setItem('groceries', JSON.stringify(this.groceries))
   }
 }
 </script>
+<style scoped>
+  .logo {
+    margin-bottom: 30%;
+  }
+  .form-container {
+    background-color: #426E5E;
+  }
+  .table-container {
+    background-color: #FDF6E1;
+  }
+  .h-100 {
+    height: 100%;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
 import NotFound from './views/NotFound.vue'
 
 const routes = [
     { path: '/', name: 'App', component: App },
-    { path: '/home', name: 'Home', component: Home },
-    { path: '/about', name: 'about', component: About },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 const router = createRouter({
